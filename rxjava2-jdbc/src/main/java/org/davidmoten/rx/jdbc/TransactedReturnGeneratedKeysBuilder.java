2a3c9fe407e8fae45075da07d190e0ae2980d101
Konrad Durnoga <konrad.durnoga@connectpoint.pl>
2018-04-10 00:23:25 +0200

    Transaction reference count fixed

--- rxjava2-jdbc/src/main/java/org/davidmoten/rx/jdbc/TransactedReturnGeneratedKeysBuilder.java
+++ rxjava2-jdbc/src/main/java/org/davidmoten/rx/jdbc/TransactedReturnGeneratedKeysBuilder.java
@@ -11,57 +11,65 @@ import io.reactivex.Flowable;
 
 public final class TransactedReturnGeneratedKeysBuilder implements GetterTx {
 
-    private final TransactedUpdateBuilder update;
-    private final Database db;
+	private final TransactedUpdateBuilder update;
+	private final Database db;
 
-    TransactedReturnGeneratedKeysBuilder(TransactedUpdateBuilder update, Database db) {
-        this.update = update;
-        this.db = db;
-    }
+	TransactedReturnGeneratedKeysBuilder(TransactedUpdateBuilder update, Database db) {
+		this.update = update;
+		this.db = db;
+	}
 
-    /**
-     * Transforms the results using the given function.
-     *
-     * @param mapper
-     *            maps the query results to an object
-     * @return the results of the query as an Observable
-     */
-    @Override
-    public <T> Flowable<Tx<T>> get(@Nonnull ResultSetMapper<? extends T> mapper) {
-        Preconditions.checkNotNull(mapper, "mapper cannot be null");
-        return Flowable.defer(() -> {
-            AtomicReference<Connection> connection = new AtomicReference<Connection>();
-            Flowable<T> o = Update.<T>createReturnGeneratedKeys( //
-                    update.updateBuilder.connections //
-                            .map(c -> Util.toTransactedConnection(connection, c)),
-                    update.parameterGroupsToFlowable(), update.updateBuilder.sql, mapper, false);
-            return o.materialize() //
-                    .flatMap(n -> Tx.toTx(n, connection.get(), db)) //
-                    .doOnNext(tx -> {
-                        if (tx.isComplete()) {
-                            ((TxImpl<T>) tx).connection().commit();
-                        }
-                    });
-        });
-    }
+	/**
+	 * Transforms the results using the given function.
+	 *
+	 * @param mapper
+	 *            maps the query results to an object
+	 * @return the results of the query as an Observable
+	 */
+	@Override
+	public <T> Flowable<Tx<T>> get(@Nonnull ResultSetMapper<? extends T> mapper) {
+		Preconditions.checkNotNull(mapper, "mapper cannot be null");
+		return Flowable.defer(() -> {
+			AtomicReference<Connection> connection = new AtomicReference<Connection>();
+			Flowable<T> o = Update.<T>createReturnGeneratedKeys( //
+					update.updateBuilder.connections //
+							.map(c -> Util.toTransactedConnection(connection, c)),
+					update.parameterGroupsToFlowable(), update.updateBuilder.sql, mapper, false);
+			TxImpl<?>[] t = new TxImpl[1];
+			return o.materialize() //
+					.flatMap(n -> Tx.toTx(n, connection.get(), db)) //
+					.doOnNext(tx -> {
+						t[0] = ((TxImpl<?>) tx);
+					})
+					.doOnComplete(() -> {
+						TxImpl<?> tx = t[0];
+						if (tx.isComplete()) {
+							tx.connection()
+									.commit();
+						}
+						Util.closeSilently(tx.connection());
+					});
+		});
+	}
 
-    public ValuesOnly valuesOnly() {
-        return new ValuesOnly(this);
-    }
+	public ValuesOnly valuesOnly() {
+		return new ValuesOnly(this);
+	}
 
-    public static final class ValuesOnly implements Getter {
+	public static final class ValuesOnly implements Getter {
 
-        private final TransactedReturnGeneratedKeysBuilder builder;
+		private final TransactedReturnGeneratedKeysBuilder builder;
 
-        public ValuesOnly(TransactedReturnGeneratedKeysBuilder builder) {
-            this.builder = builder;
-        }
+		public ValuesOnly(TransactedReturnGeneratedKeysBuilder builder) {
+			this.builder = builder;
+		}
 
-        @Override
-        public <T> Flowable<T> get(ResultSetMapper<? extends T> function) {
-            return builder.get(function).flatMap(Tx.flattenToValuesOnly());
-        }
+		@Override
+		public <T> Flowable<T> get(ResultSetMapper<? extends T> function) {
+			return builder.get(function)
+					.flatMap(Tx.flattenToValuesOnly());
+		}
 
-    }
+	}
 
 }
