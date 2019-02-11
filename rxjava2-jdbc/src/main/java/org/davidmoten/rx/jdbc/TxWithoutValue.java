c795f43245aa6fada65fa0a52d6660c59006d3c4
Konrad Durnoga <konrad.durnoga@connectpoint.pl>
2019-02-11 12:01:00 +0100

    Transaction rollback support added

--- rxjava2-jdbc/src/main/java/org/davidmoten/rx/jdbc/TxWithoutValue.java
+++ rxjava2-jdbc/src/main/java/org/davidmoten/rx/jdbc/TxWithoutValue.java
@@ -1,11 +1,17 @@
 package org.davidmoten.rx.jdbc;
 
+import java.sql.SQLException;
+
 public interface TxWithoutValue {
 
     boolean isComplete();
 
     boolean isError();
 
+    void commit() throws SQLException;
+    
+    void rollback() throws SQLException;
+    
     Throwable throwable();
     
     TransactedSelectBuilder select(String sql);
